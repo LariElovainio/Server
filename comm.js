47a45489dce703c0cdd86f9859492ef89e676246
Lari Elovainio <me@larielovainio.com>
2014-04-28 18:14:26 +0300

    Add serial communication

--- comm.js
+++ comm.js
@@ -1,8 +1,15 @@
+var SerialPort = require("serialport").SerialPort;
 var io = require('socket.io').listen(3000);
-var heart = 0;
+
+var serialPort;
+var portName = '/dev/ttyACM0';
+var sendData = "";
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
+  serialListener(debug);
+  io.sockets.emit('updateSensor', 'koira');
+  
   /*socket.on('my other event', function (data) {
     console.log(data);
   });
@@ -17,4 +24,35 @@ io.sockets.on('connection', function (socket) {
 		console.log(data);
 		io.sockets.emit('update', data);
 	});
+	socket.on('sensorData', function (data) {
+		console.log(data);
+		io.sockets.emit('updateSensor', data);
+	});
 });
+
+function serialListener(debug)
+{
+    var receivedData = "";
+    serialPort = new SerialPort(portName, {
+        baudrate: 9600,
+        // defaults for Arduino serial communication
+         dataBits: 8,
+         parity: 'none',
+         stopBits: 1,
+         flowControl: false
+    });
+
+    serialPort.on("open", function () {
+      console.log('open serial communication');
+            // Listens to incoming data
+        serialPort.on('data', function(data) {
+             receivedData += data.toString();
+          if (receivedData .indexOf('E') >= 0 && receivedData .indexOf('B') >= 0) {
+           sendData = receivedData .substring(receivedData .indexOf('B') + 1, receivedData .indexOf('E'));
+           receivedData = '';
+         }
+         // send the incoming data to browser with websockets.
+       socketServer.emit('update', sendData);
+      });
+    });
+}
